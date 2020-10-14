a8f2596d1f775f4b9fa2e765261f570784afa9a3
phillip.hqs <63358485+philliphqs@users.noreply.github.com>
2020-10-14 02:39:44 +0200

    Uploading assets
    Images:
    
    1. Titles
    2. Logos
    3. Safey logos
    4. Icons
    
    Other:
    
    1. Font: arial.ttf
    2. properties.py

---
+++ assets/properties.py
@@ -0,0 +1,113 @@
+ProductName = 'Safey'
+ProductDescription = 'Safey is a anonym chat client using Python'
+ProductVersion = '0.0.1 beta'
+ProductAuthor = 'philliphqs'
+ProductOrganisation = 'hqsartworks'
+ProductRepo = f'https://github.com/{ProductAuthor}/{ProductName}'
+ProductLanguages = ['tigrinya', 'amharic', 'german', 'english', 'tuerkiye']
+
+SupportedOS = 'Windows'
+
+hqsartworksDiscordInvite = 'https://discord.com/invite/2zBUbV9'
+AlphaclanDiscordInvite = 'https://discord.com/invite/uFdVUMH'
+
+DefaultTheme = 'Dark'
+DefaultFont = 'assets/arial.ttf'
+
+Domain = 'hqsartworks.me'
+SafeyServerIP = f'safey.{Domain}'
+AlphaclanServerIP = f'alphaclan.{Domain}'
+Website = f'https://{Domain}/'
+UpdateServer = f'https://update.{Domain}/safey'
+
+DevInstagram = 'phillip.60528'
+DevSnapchat = 'phillip.hqs'
+DevTwitter = 'phillip_hqs'
+DevGitHub = 'philliphqs'
+
+OrgInstagram = 'hqsartworks'
+OrgSnapchat = 'hqsartworks'
+OrgTwitter = 'hqsartworks'
+OrgGitHub = 'hqsartworks'
+
+Themes = ['Dark', 'Classic', 'Grey', 'Gold', 'Red']
+Languages = ['german', 'english', 'tigrinya', 'amharic', 'tuerkiye']
+
+AssetsIconPath = 'assets/icons/'
+AssetsLogoPath = 'assets/logos/'
+AssetsSafeyPath = 'assets/Safey/'
+AssetsTitleEnglishPath = 'assets/title/english/'
+AssetsTitleGermanPath = 'assets/title/german/'
+
+ErrorIcon = f'{AssetsIconPath}error.png'
+InfoIcon = f'{AssetsIconPath}info.png'
+WarningIcon = f'{AssetsIconPath}warning.png'
+SecurityalertIcon = f'{AssetsIconPath}securityalert.png'
+
+hqsartworksLogo = f'{AssetsLogoPath}hqsartworksLogo.png'
+AlphaclanLogo = f'{AssetsLogoPath}AlphaclanLogo.png'
+
+DiscordLogo = f'{AssetsLogoPath}DiscordLogo.png'
+InstagramLogo = f'{AssetsLogoPath}InstagramLogo.png'
+SnapchatLogo = f'{AssetsLogoPath}SnapchatLogo.png'
+TwitterLogo = f'{AssetsLogoPath}TwitterLogo.png'
+
+hqsartworksLogoWord = f'{AssetsLogoPath}hqsartworksLogoWord.png'
+AlphaclanLogoWord = f'{AssetsLogoPath}AlphaclanLogoWord.png'
+
+DiscordLogoWord = f'{AssetsLogoPath}DiscordLogoWord.png'
+hqsbotLogoWord = f'{AssetsLogoPath}hqsbotLogoWord.png'
+
+SafeyIconDark = f'{AssetsSafeyPath}SafeyLogoDark.ico'
+SafeyIconLight = f'{AssetsSafeyPath}SafeyLogoLight.ico'
+
+SafeyLogoDark = f'{AssetsSafeyPath}SafeyLogoDark.png'
+SafeyLogoLight = f'{AssetsSafeyPath}SafeyLogoLight.png'
+
+SafeyLogoWordDark = f'{AssetsSafeyPath}SafeyLogoWordDark.png'
+SafeyLogoWordLight = f'{AssetsSafeyPath}SafeyLogoWordLight.png'
+
+TitleEnglishDarkAbout = f'{AssetsTitleEnglishPath}dark/about.png'
+TitleEnglishDarkQandA = f'{AssetsTitleEnglishPath}dark/q&a.png'
+TitleEnglishDarkSettings = f'{AssetsTitleEnglishPath}dark/settings.png'
+TitleEnglishDarkTutorial = f'{AssetsTitleEnglishPath}dark/tutorial.png'
+
+TitleEnglishLightAbout = f'{AssetsTitleEnglishPath}light/about.png'
+TitleEnglishLightQandA = f'{AssetsTitleEnglishPath}light/q&a.png'
+TitleEnglishLightSettings = f'{AssetsTitleEnglishPath}light/settings.png'
+TitleEnglishLightTutorial = f'{AssetsTitleEnglishPath}light/tutorial.png'
+
+TitleGermanDarkAbout = f'{AssetsTitleGermanPath}dark/über.png'
+TitleGermanDarkQandA = f'{AssetsTitleEnglishPath}dark/q&a.png'
+TitleGermanDarkSettings = f'{AssetsTitleGermanPath}dark/einstellungen.png'
+TitleGermanDarkTutorial = f'{AssetsTitleGermanPath}dark/anleitung.png'
+
+TitleGermanLightAbout = f'{AssetsTitleGermanPath}light/über.png'
+TitleGermanLightQandA = f'{AssetsTitleEnglishPath}light/q&a.png'
+TitleGermanLightSettings = f'{AssetsTitleGermanPath}light/einstellungen.png'
+TitleGermanLightTutorial = f'{AssetsTitleGermanPath}light/anleitung.png'
+
+DiscordRPCClientID = '764883045349457940'
+DiscordRPCState = ProductOrganisation
+DiscordRPCDetails = ProductVersion
+DiscordRPCSmallImage = 'hqsartworkslogobackground'
+DiscordRPCLargeImage = 'safeylogodark'
+DiscordRPCLargeText = f'{ProductName}, {ProductVersion}, {ProductAuthor}'
+DiscordRPCSmallText = f'{ProductOrganisation}, {ProductAuthor}'
+
+LanguagesEnglish = f'assets/english.hqs'
+LanguagesGerman = f'assets/german.hqs'
+LanguagesTigrinya = f'assets/tigrinya.hqs'
+LanguagesAmharic = f'assets/amharic.hqs'
+LanguagesTuerkiye = f'assets/tuerkiye.hqs'
+
+LanguageErrorCodes = ['l0000', 'l0001']
+ThemeErrorCodes = ['t0000']
+ChatErrorCodes = ['c0000', 'c0001', 'c0002']
+
+JailbreakInviteLink = 'https://discord.gg/MB6SSPR'
+JailbreakServerIP = f'jailbreak.{Domain}'
+JailbreakLogo = f'{AssetsLogoPath}JailbreakLogo.png'
+
+MainWindowWidgets = ['ListedServerWindow', 'ChatWidget', 'ChatWidgetMessage', 'ServerDescriptionWidget']
+
